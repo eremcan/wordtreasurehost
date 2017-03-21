33798fc37255c402bd8515b4f289439010ae8e5e
Çağatay EREM <cagatayerem@gmail.com>
2017-03-21 21:29:23 +0300

    deneme deneme..

--- src/test/java/com/word/WordApplicationTests.java
+++ src/test/java/com/word/WordApplicationTests.java
@@ -2,15 +2,31 @@ package com.word;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static junit.framework.TestCase.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WordApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void testGreeting() throws Exception {
+        ResponseEntity<String> entity = restTemplate
+                .getForEntity("http://localhost:" + this.port + "/", String.class);
+        assertEquals(HttpStatus.OK, entity.getStatusCode());
+    }
 
 }
